1097aff8f084daae278d374075a1da6fed891e26
Lebogang Mokoena <ditswe24@gmail.com>
2026-06-20 20:56:07 +0200

    add genre definitions map from CodeSpace data file

--- src/data.js
+++ src/data.js
@@ -1,1 +1,73 @@
+// Genre data provided by CodeSpace Academy.
+// All other podcast information is fetched from: https://podcast-api.netlify.app/
 
+export const genres = [
+  {
+    id: 1,
+    title: 'Personal Growth',
+    description:
+      'Looking to improve yourself and reach your full potential? Look no further than our collection of personal growth podcasts.',
+    shows: ['10716', '10276', '6756', '10660'],
+  },
+  {
+    id: 2,
+    title: 'Investigative Journalism',
+    description:
+      'In-depth reporting and analysis of some of the most important and intriguing stories of our time.',
+    shows: [
+      '10716', '5675', '10539', '9177', '8860', '5012', '9054', '7654',
+      '8256', '8291', '5718', '5276', '5964', '6465', '5320', '6451', '5692', '6430',
+    ],
+  },
+  {
+    id: 3,
+    title: 'History',
+    description:
+      'Experience the past like never before with our collection of history podcasts.',
+    shows: ['5279', '9177', '6807', '8514', '5629', '8364', '5964', '9041', '5702', '5320', '6717', '5968', '8760'],
+  },
+  {
+    id: 4,
+    title: 'Comedy',
+    description: 'Get ready to laugh with our collection of comedy podcasts.',
+    shows: ['6807'],
+  },
+  {
+    id: 5,
+    title: 'Entertainment',
+    description:
+      'Entertainment lovers, get ready for a treat! Our collection of entertainment podcasts covers it all.',
+    shows: ['6807', '6631', '8256', '6756', '5702', '9620', '10758'],
+  },
+  {
+    id: 6,
+    title: 'Business',
+    description:
+      'In-depth analysis and expert insight into the business side of the entertainment world.',
+    shows: ['8364', '6717', '8760'],
+  },
+  {
+    id: 7,
+    title: 'Fiction',
+    description:
+      'Get ready to be transported to new worlds with our collection of fiction-focused podcasts.',
+    shows: ['6631', '9664', '8188', '9702', '9695', '9994', '9263', '9666'],
+  },
+  {
+    id: 8,
+    title: 'News',
+    description:
+      'Stay informed and on top of the latest events with our collection of current news podcasts.',
+    shows: ['8291', '5718'],
+  },
+  {
+    id: 9,
+    title: 'Kids and Family',
+    description:
+      'Bring some fun and learning to your family with our collection of kids and family podcasts.',
+    shows: [
+      '8188', '9687', '9702', '9665', '10182', '9994', '9705', '10934',
+      '9694', '9693', '9704', '9739', '9691',
+    ],
+  },
+];
